da2862e6aba7cb1cedf8cc1fd28ee952d84e0553
letuan1206 <tuanleanh1994@gmail.com>
2017-10-21 23:51:21 +0700

    Don't accept order product trial if user has use order product trial … (#4)

--- controllers/order.js
+++ controllers/order.js
@@ -4,6 +4,11 @@ var OrderDetail = require('../models/OrderDetail');
 var OrderAddress = require('../models/OrderAddress');
 var cityList = require('../models/City');
 
+/**
+ * method: POST
+ * url: api/order
+ * params:
+ */
 exports.orderProduct = function(req, res, next) {
   req.assert('user_id', 'User is required').notEmpty();
   req.assert('address', 'Adress is required').notEmpty();
@@ -20,40 +25,55 @@ exports.orderProduct = function(req, res, next) {
 
   var data = req.body;
 
-  new Order({
-    user_id: data.user_id,
-    status: 0
-  })
-  .save()
-  .then(function(order) {
-    var addressParams = data.address;
-    addressParams.order_id = order.id;
-    new OrderAddress(addressParams)
-      .save()
-      .then(function(orderAddress) {
-
-      })
-      .catch(function(err) {
-        return res.status(401).send(err);
-      })
-
-    var orderDetailParams = data.product_list.map(result => {
-      result.order_id = order.id;
-      new OrderDetail(result)
+  new Order()
+    .query(function (qb) {
+      qb.select('user_id', 'product_id', 'is_trial')
+        .leftJoin('order_details', 'order_details.order_id', 'orders.id')
+        .leftJoin('products', 'products.id', 'order_details.product_id')
+        .where('orders.user_id', data.user_id)
+        .where('is_trial', 1)
+    })
+    .count()
+    .then(function (rs) {
+      if(rs) {
+        return res.status(400).send({ msg: 'You has been order product trial. Please don\'t order product trial again ' });
+      } else {
+        new Order({
+          user_id: data.user_id,
+          status: 0
+        })
         .save()
-        .then(function (orderDetail) {
+        .then(function (order) {
+          var addressParams = data.address;
+          addressParams.order_id = order.id;
+          new OrderAddress(addressParams)
+            .save()
+            .then(function (orderAddress) {
 
+            })
+            .catch(function (err) {
+              return res.status(401).send(err);
+            })
+
+          var orderDetailParams = data.product_list.map(result => {
+            result.order_id = order.id;
+            new OrderDetail(result)
+              .save()
+              .then(function (orderDetail) {
+
+              })
+              .catch(function (err) {
+                return res.status(401).send(err);
+              })
+          })
+
+          return res.send({ msg: 'Your order has been successfully placed!' });
         })
         .catch(function (err) {
           return res.status(401).send(err);
         })
+      }
     })
-
-    return res.send({ msg: 'Your order has been successfully placed!' });
-  })
-  .catch(function(err) {
-    return res.status(401).send(err);
-  })
 }
 
 exports.getListAll = function(req, res, next) {
